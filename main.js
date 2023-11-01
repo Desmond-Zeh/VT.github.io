049772e4d47b011fa99989dc8022fd47290ef4fc
Desmond-Zeh <148614390+Desmond-Zeh@users.noreply.github.com>
2023-11-02 00:50:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+/* Open a new window to display the following document links when clicked on the DOCUMENT tab in the Navigation bar*/
+
+let mydoc = document.getElementById("myDoc");
+mydoc.onclick = () =>{
+window.location.replace("https://desmond-zeh.github.io/Des.github.io/","_blank");
+};
+ let myhom = document.getElementById("home");
+myhom.onclick = () =>{
+window.location.replace("https://desmond-zeh.github.io/Zeh-home.github.io/", "_blank");
+};
+
+let mydown = document.getElementById("down");
+mydown.onclick = () =>{
+window.location.replace("https://desmond-zeh.github.io/Des.github.io/");
+};
+ let myabout = document.getElementById("about");
+myabout.onclick = () =>{
+window.location.replace("");
+};
